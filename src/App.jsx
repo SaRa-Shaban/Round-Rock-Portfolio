0d73f0b93b732ae6ae2a6e5b63d778087a9997f3
sara shaban <eng.sara6542@gmail.com>
2023-10-19 02:34:31 +0300

    fetch testimonials api and execute pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import reactLogo from "./assets/react.svg";
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import '@fortawesome/fontawesome-free/css/all.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./App.css";
 import Home from "./Components/Home/Home";
 import AboutUs from "./Components/AboutUs/AboutUs";
@@ -9,18 +9,26 @@ import Services from "./Components/Services/Services";
 import Testimonials from "./Components/Testimonials/Testimonials";
 import ContactUs from "./Components/ContactUs/ContactUs";
 import Navbar from "./Components/Navbar/Navbar";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+// Create a client
+const queryClient = new QueryClient()
 
 
 function App() {
-
-  return <>
-  <Home/>
-  <AboutUs/>
-  <Services/>
-  <Testimonials/>
-  <ContactUs/>
-  <Navbar/>
-  </>;
+  return (
+    <>
+      <Home />
+      <AboutUs />
+      <Services />
+      {/*  Provide the client to your App */}
+      <QueryClientProvider client={queryClient}>
+        <Testimonials />
+      </QueryClientProvider>
+      <ContactUs />
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Testimonials.module.css";
 import client from "./../../assets/Testimonials/client1.png";
+import axios from "axios";
+import { useQuery } from "react-query";
 
 export default function Testimonials() {
+  const [page, setPage] = useState(1);
+  const totalPages = 2;
+  // console.log(page);
+
+  const getTestimonials = async () => {
+    const response = await axios.get(
+      `https://reqres.in/api/users?page=${page}`
+    );
+    console.log(response);
+    return response.data.data;
+  };
+
+  const { isLoading, isError, error, data } = useQuery(
+    ["users", page],
+    getTestimonials
+  );
+
+  // console.log(data);
+
+  if (isLoading) {
+    return <div className="globalContainer fs-1">Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <>
       <div className="container globalContainer pb-5">
@@ -14,51 +43,88 @@ export default function Testimonials() {
         </div>
 
         <div className="row g-4">
-          <div className="col-lg-6 col-md-12">
-            <div className={style.content}>
-              <div className="card border-0 p-1 rounded-5">
-                <div className="row g-0">
-                  <div className="col-md-3 text-center py-3">
-                    <img
-                      src={client}
-                      className="img-rounded rounded-center"
-                      alt="client"
-                    />
-                  </div>
-                  <div className="col-md-9">
-                    <div className={`${style.card} card-body`}>
-                      <h5 className="card-title">Courtney Henry</h5>
-                      <p className="card-text">
-                        Lorem Ipsum is simply dummy text of the printing and
-                        typesetting industry. Lorem Ipsum has been the
-                        industry's standard dummy text ever since the 1500s,
-                        when an unknown printer took a galley of type and
-                        scrambled it to make a type specimen book
-                      </p>
+          {data?.map((user) => (
+            <div className="col-lg-6 col-md-12" key={user.id}>
+              <div className={style.content}>
+                <div className="card border-0 p-1 rounded-5">
+                  <div className="row g-0">
+                    <div className="col-md-3 text-center py-3">
+                      <img
+                        src={user.avatar}
+                        className="img-rounded rounded-center"
+                        alt="client"
+                      />
+                    </div>
+                    <div className="col-md-9">
+                      <div className={`${style.card} card-body`}>
+                        <h5 className="card-title">
+                          {user.first_name} {user.last_name}
+                        </h5>
+                        <p className="card-text">
+                          Lorem Ipsum is simply dummy text of the printing and
+                          typesetting industry. Lorem Ipsum has been the
+                          industry's standard dummy text ever since the 1500s,
+                          when an unknown printer took a galley of type and
+                          scrambled it to make a type specimen book
+                        </p>
 
-                      <i className="fa-solid fa-star fa-sm ps-1"></i>
-                      <i className="fa-solid fa-star fa-sm ps-1"></i>
-                      <i className="fa-solid fa-star fa-sm ps-1"></i>
-                      <i className="fa-solid fa-star fa-sm ps-1"></i>
-                      <i className="fa-solid fa-star fa-sm ps-1"></i>
+                        {/* <div className="d-flex justify-content-start align-items-center pb-3 pt-2">
+                          <i
+                            className={`fa-solid fa-envelope h-100 ${style.iconColor}`}
+                          ></i>
+                          <a
+                            className={`ps-3 ${style.customLink}`}
+                            href="mailto:upskilling.eg1@gmail.com"
+                          >
+                            upskilling.eg1@gmail.com
+                          </a>
+                        </div> */}
+
+                        <i className="fa-solid fa-star fa-sm ps-1"></i>
+                        <i className="fa-solid fa-star fa-sm ps-1"></i>
+                        <i className="fa-solid fa-star fa-sm ps-1"></i>
+                        <i className="fa-solid fa-star fa-sm ps-1"></i>
+                        <i className="fa-solid fa-star fa-sm ps-1"></i>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          ))}
 
+          {/* pagination buttons */}
           <div className="d-flex justify-content-center align-items-center mt-5">
-                <nav aria-label="Page navigation example ">
-                  <ul className="pagination">
-                  <li className="page-item"><a className="page-link" href="#">
+            <nav aria-label="Page navigation example ">
+              <ul className="pagination">
+                <li className="page-item">
+                  <button
+                    type="button"
+                    className={`page-link ${style.btnPagination}`}
+                    onClick={() => {
+                      setPage(page - 1);
+                      // console.log(page);
+                    }}
+                    disabled={page === 1}
+                  >
                     <i className="fa-solid fa-angles-left" />
-                    </a></li>
-                  <li className="page-item"><a className="page-link" href="#">
-                  <i className="fa-solid fa-angles-right" />
-                </a></li>
-                  </ul>
-                </nav>
+                  </button>
+                </li>
+                <li className="page-item">
+                  <button
+                    type="button"
+                    className={`page-link ${style.btnPagination}`}
+                    onClick={() => {
+                      setPage(page + 1);
+                      // console.log(page);
+                    }}
+                    disabled={page === totalPages}
+                  >
+                    <i className="fa-solid fa-angles-right" />
+                  </button>
+                </li>
+              </ul>
+            </nav>
           </div>
         </div>
       </div>
